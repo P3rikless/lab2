57a8fe324e69e2017d2e8eec08ec2d896ff238ff
Petru Ciulei <ciuleipetru10@gmail.com>
2026-03-03 13:33:13 +0200

    cod bun

--- lab2.c
+++ lab2.c
@@ -3,19 +3,27 @@
 
 int function(int x)
 {
+	if(x>0)
+	{
 	x=x-2;
-	//x=function(x);
+	x=function(x);
+	}	
 	return x;
 }
 
 int main(){
-	int a=30, i=0;
+	int a=30,i=0;
 	
-	while (a>0){
+while (a>0)
+{
 		a=function(a);
 		i++;
 	}
-	printf("\n a/2= %d \n\n",i);
+
+	
+	
+	
+	printf("\n %d \n\n",i);
 	return 0;	
 }
 
